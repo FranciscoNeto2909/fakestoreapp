c1772f591782559a9d59e912fb72f643175206ea
Francisco Neto <wantedshark@gmail.com>
2022-06-29 13:35:58 -0300

    create build

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,7 +2,7 @@ export default function About() {
     return(
         <>
         <h3>About</h3>
-        <p>Aplicação desenvolvida por mim, Francisco Neto, para praticar requisições a apis. A api utilizada nesse projeto é <a href="https://fakestoreapi.com/" target="_blank">fakestore.api</a>.</p>
+        <p>Aplicação desenvolvida por mim, Francisco Neto, para praticar requisições a apis. A api utilizada nesse projeto é <a href="https://fakestoreapi.com/" target="_blank" rel="noreferrer">fakestore.api</a>.</p>
         </>
     )
 }
